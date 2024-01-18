edda3dccbfdf601c400e26a849adee077da501f4
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-01-18 11:26:00 +0100

    MySQl Advance

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- SQL script that create table users with certain requirements 
+
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
+
