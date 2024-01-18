d4958910be980fdbacf2dacd0eadd61afd4461fd
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-01-18 12:14:12 +0100

    MySQL Advance

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- A SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 
 SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
-WHERE stle LIKE '%GLAM rock%' ORDER BY lifespan DESC;
+WHERE style LIKE '%GLAM rock%' ORDER BY lifespan DESC;
